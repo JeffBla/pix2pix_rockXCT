ae59ed20238da7c2c42f98da07d864616c2ef48c
JeffBla <jeff920305@gmail.com>
2023-09-20 19:14:34 +0800

    :sparkles: (resnet_precent) try to train with resnet
    change the percentlayer, so we now use resnet. Resnet seems quite good with lsgan

--- models/networks.py
+++ models/networks.py
@@ -9,6 +9,8 @@ from torch.optim import lr_scheduler
 
 from .unet_model import UNet
 
+from .percentlayer import PercentLayer
+
 ###############################################################################
 # Helper Functions
 ###############################################################################
@@ -194,9 +196,9 @@ def define_G_percent(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=F
     norm_layer = get_norm_layer(norm_type=norm)
 
     if netG == 'resnet_9blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
+        net = ResnetGenerator_percent(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9, solid_ct=solid_ct, water_ct=water_ct, air_ct=air_ct)
     elif netG == 'resnet_6blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
+        net = ResnetGenerator_percent(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6, solid_ct=solid_ct, water_ct=water_ct, air_ct=air_ct)
     elif netG == 'unet_128':
         net = UnetGenerator_percent(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout, solid_ct=solid_ct, water_ct=water_ct, air_ct=air_ct)
     elif netG == 'unet_256':
@@ -425,6 +427,67 @@ class ResnetGenerator(nn.Module):
         """Standard forward"""
         return self.model(input)
 
+class ResnetGenerator_percent(nn.Module):
+    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
+
+    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
+    """
+
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect', solid_ct=2232.875, water_ct=0, air_ct=-1000):
+        """Construct a Resnet-based generator
+
+        Parameters:
+            input_nc (int)      -- the number of channels in input images
+            output_nc (int)     -- the number of channels in output images
+            ngf (int)           -- the number of filters in the last conv layer
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers
+            n_blocks (int)      -- the number of ResNet blocks
+            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+        """
+        assert(n_blocks >= 0)
+        super(ResnetGenerator_percent, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):  # add downsampling layers
+            mult = 2 ** i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):       # add ResNet blocks
+
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):  # add upsampling layers
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [PercentLayer(solid_ct, water_ct, air_ct)
+]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        """Standard forward"""
+        return self.model(input)
+
 
 class ResnetBlock(nn.Module):
     """Define a Resnet block"""
@@ -544,7 +607,6 @@ class UnetGenerator_percent(nn.Module):
         unet_block = UnetSkipConnectionBlock_percent(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         self.model = UnetSkipConnectionBlock_percent(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer)  # add the outermost layer
         
-        from .percentlayer import PercentLayer
         # turn percentage to ct
         self.percentLayer = PercentLayer(solid_ct, water_ct, air_ct)
 
--- models/percentlayer.py
+++ models/percentlayer.py
@@ -10,14 +10,14 @@ class PercentLayer(nn.Module):
     """3 input. 1 output"""
     def forward(self, x):
 
-        # x = torch.softmax(x, 1)
+        x = torch.softmax(x, 1)
 
-        # ct = self.CTG * x[:, 0] + self.WATER * x[:, 1] + self.AIR * x[:, 2]
-        # ct = ct.unsqueeze(1)
+        ct = self.CTG * x[:, 0] + self.WATER * x[:, 1] + self.AIR * x[:, 2]
+        ct = ct.unsqueeze(1)
 
-        # rescale = (ct - self.AIR)  / (self.CTG - self.AIR)
-        # rescale = 2 * rescale - 1
+        rescale = (ct - self.AIR)  / (3000 - self.AIR)
+        rescale = 2 * rescale - 1
         
-        tanh = nn.Tanh()
-        output = tanh(x)  # add a non-linear function
-        return output
+        # tanh = nn.Tanh()
+        # output = tanh(x)  # add a non-linear function
+        return rescale
