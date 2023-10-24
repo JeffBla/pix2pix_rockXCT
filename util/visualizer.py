66d0ad062f76a7dee85533d3517e2747993fd26f
JeffBla <jeff920305@gmail.com>
2023-10-24 17:30:02 +0800

    :sparkles: (CTG) make CGAN to adjust the CTG
    CGAN now can adjust CTG by itself. I add functions to visualize in wandb

--- util/visualizer.py
+++ util/visualizer.py
@@ -255,3 +255,7 @@ class Visualizer():
         print(message)  # print the message
         with open(self.log_name, "a") as log_file:
             log_file.write('%s\n' % message)  # save the message
+
+    def plot_current_ctg_wandb(self, ctg):  
+        if self.use_wandb:
+            self.wandb_run.log({'CTG':ctg})
